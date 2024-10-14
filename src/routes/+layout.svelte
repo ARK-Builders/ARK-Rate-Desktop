e20ed9c64a8032bc68331dd73ba860288f047210
oluiscabral <oluiscabral@hotmail.com>
2024-10-14 16:24:24 -0300

    Setup formatter and linter

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,107 +1,132 @@
 <script lang="ts">
-  import { Button, Input } from "flowbite-svelte";
-  import {
-    Bell,
-    ChartNoAxesColumn,
-    Coins,
-    PanelLeft,
-    Search,
-    Settings,
-  } from "lucide-svelte";
-  import "../app.css";
+	import { Button, Input } from 'flowbite-svelte';
+	import { Bell, ChartNoAxesColumn, Coins, PanelLeft, Search, Settings } from 'lucide-svelte';
+	import '../app.css';
 
-  let isNavigationDrawerOpen = true;
-  const toggleNavigationDrawer = () =>
-    (isNavigationDrawerOpen = !isNavigationDrawerOpen);
+	let isNavigationDrawerOpen = true;
+	const toggleNavigationDrawer = () => (isNavigationDrawerOpen = !isNavigationDrawerOpen);
 </script>
 
-<div class="flex box-border min-w-full min-h-screen">
-  <aside
-    class="transition-all border-r {isNavigationDrawerOpen ? ' w-64' : ' w-16'}"
-  >
-    {#if isNavigationDrawerOpen}
-      <div class="flex flex-col gap-4 py-10 px-4">
-        <div class="flex justify-between items-center px-4">
-          <img class="size-14" src="/md-logo.png" alt="Ark Rate Logo" />
-          <Button
-            size="xs"
-            outline
-            color="light"
-            on:click={toggleNavigationDrawer}
-          >
-            <PanelLeft class="size-5" />
-          </Button>
-        </div>
-        <Input type="search" placeholder="Search" size="md">
-          <Search slot="left" class="size-5" />
-        </Input>
-        <Button
-          outline
-          size="sm"
-          color="primary"
-          class="justify-start gap-2 px-3 shadow-none border-none"
-        >
-          <Coins class="size-5" />
-          Quick
-        </Button>
-        <Button
-          outline
-          size="sm"
-          color="primary"
-          class="justify-start gap-2 px-3 shadow-none border-none"
-        >
-          <ChartNoAxesColumn class="size-5" />
-          Portfolios
-        </Button>
-        <Button
-          outline
-          size="sm"
-          color="primary"
-          class="justify-start gap-2 px-3 shadow-none border-none"
-        >
-          <Bell class="size-5" />
-          Alerts
-        </Button>
-        <Button
-          outline
-          size="sm"
-          color="primary"
-          class="justify-start gap-2 px-3 shadow-none border-none"
-        >
-          <Settings class="size-5" />
-          Settings
-        </Button>
-      </div>
-    {:else}
-      <div class="flex flex-col gap-4 py-10 px-4 items-center">
-        <img class="size-8" src="/sm-logo.png" alt="Ark Rate Logo" />
-        <Button
-          size="xs"
-          outline
-          color="light"
-          on:click={toggleNavigationDrawer}
-        >
-          <PanelLeft class="size-4" />
-        </Button>
-        <Button size="xs" outline color="light">
-          <Search class="size-4" />
-        </Button>
-        <Button size="xs" outline color="primary">
-          <Coins class="size-4" />
-        </Button>
-        <Button size="xs" outline color="primary">
-          <ChartNoAxesColumn class="size-4" />
-        </Button>
-        <Button size="xs" outline color="primary">
-          <Bell class="size-4" />
-        </Button>
-        <Button size="xs" outline color="primary">
-          <Settings class="size-4" />
-        </Button>
-      </div>
-    {/if}
-  </aside>
-  <div class="flex-grow">
-    <slot />
-  </div>
+<div class="box-border flex min-h-screen min-w-full">
+	<aside class="border-r transition-all {isNavigationDrawerOpen ? ' w-64' : ' w-16'}">
+		{#if isNavigationDrawerOpen}
+			<div class="flex flex-col gap-4 px-4 py-10">
+				<div class="flex items-center justify-between px-4">
+					<img
+						class="size-14"
+						src="/md-logo.png"
+						alt="Ark Rate Logo"
+					/>
+					<Button
+						outline
+						size="xs"
+						color="light"
+						on:click={toggleNavigationDrawer}
+					>
+						<PanelLeft class="size-5" />
+					</Button>
+				</div>
+				<Input
+					size="md"
+					type="search"
+					placeholder="Search"
+				>
+					<Search
+						slot="left"
+						class="size-5"
+					/>
+				</Input>
+				<Button
+					outline
+					size="sm"
+					color="primary"
+					class="justify-start gap-2 border-none px-3 shadow-none"
+				>
+					<Coins class="size-5" />
+					Quick
+				</Button>
+				<Button
+					outline
+					size="sm"
+					color="primary"
+					class="justify-start gap-2 border-none px-3 shadow-none"
+				>
+					<ChartNoAxesColumn class="size-5" />
+					Portfolios
+				</Button>
+				<Button
+					outline
+					size="sm"
+					color="primary"
+					class="justify-start gap-2 border-none px-3 shadow-none"
+				>
+					<Bell class="size-5" />
+					Alerts
+				</Button>
+				<Button
+					outline
+					size="sm"
+					color="primary"
+					class="justify-start gap-2 border-none px-3 shadow-none"
+				>
+					<Settings class="size-5" />
+					Settings
+				</Button>
+			</div>
+		{:else}
+			<div class="flex flex-col items-center gap-4 px-4 py-10">
+				<img
+					class="size-8"
+					src="/sm-logo.png"
+					alt="Ark Rate Logo"
+				/>
+				<Button
+					outline
+					size="xs"
+					color="light"
+					on:click={toggleNavigationDrawer}
+				>
+					<PanelLeft class="size-4" />
+				</Button>
+				<Button
+					outline
+					size="xs"
+					color="light"
+				>
+					<Search class="size-4" />
+				</Button>
+				<Button
+					outline
+					size="xs"
+					color="primary"
+				>
+					<Coins class="size-4" />
+				</Button>
+				<Button
+					outline
+					size="xs"
+					color="primary"
+				>
+					<ChartNoAxesColumn class="size-4" />
+				</Button>
+				<Button
+					outline
+					size="xs"
+					color="primary"
+				>
+					<Bell class="size-4" />
+				</Button>
+				<Button
+					outline
+					size="xs"
+					color="primary"
+				>
+					<Settings class="size-4" />
+				</Button>
+			</div>
+		{/if}
+	</aside>
+	<div class="flex-grow">
+		<slot />
+	</div>
 </div>
