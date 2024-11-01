61851f9442a426c88db75b005d17f51f6f96c39b
oluiscabral <oluiscabral@hotmail.com>
2024-11-01 10:39:00 -0300

    Initialize portfolios

---
+++ src/routes/portfolios/+page.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+  import { Spinner } from 'flowbite-svelte';
+  import EmptyView from './EmptyView.svelte';
+
+  let isLoading = false;
+
+  const onSaveAssetOpen = () => {
+    
+  }
+</script>
+
+{#if isLoading}
+  <div class="flex size-full items-center justify-center">
+    <Spinner class="size-32" />
+  </div>
+{:else}
+  <div class="h-full min-h-max w-full min-w-max overflow-auto p-24">
+    {#if true}
+      <EmptyView {onSaveAssetOpen} />
+    {:else}
+      <!-- PASS -->
+    {/if}
+  </div>
+{/if}
