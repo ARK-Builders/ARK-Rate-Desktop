1c56e8f486d1ba0fb1e0fa38724a759a97472a9f
oluiscabral <oluiscabral@hotmail.com>
2024-11-29 10:21:45 -0300

    Create `store_watchlist_coins` interactor

--- src-tauri/src/interactors.rs
+++ src-tauri/src/interactors.rs
@@ -5,6 +5,7 @@ pub mod interactor;
 pub mod save_pair_group;
 pub mod save_tag;
 pub mod store_portfolios;
+pub mod store_watchlist_coins;
 pub mod update_pair_group;
 pub mod update_portfolio;
 pub mod view_pair_groups;
---
+++ src-tauri/src/interactors/store_watchlist_coins.rs
@@ -0,0 +1,62 @@
+use chrono::Utc;
+use serde::Deserialize;
+use uuid::Uuid;
+
+use crate::{
+    entities::{pair::Pair, watchlist::Watchlist},
+    utilities::coin_market::CoinMarket,
+    Error,
+};
+
+use super::interactor::Interactor;
+
+pub trait StoreWatchlistCoinsDataAccess {
+    async fn get_watchlist(&mut self) -> Result<Watchlist, Error>;
+    async fn update_watchlist(&mut self, watchlist: &Watchlist) -> Result<(), Error>;
+}
+
+#[derive(Clone, Debug, Deserialize)]
+pub struct StoreWatchlistCoinsRequest {
+    pub coins: Vec<String>,
+}
+
+pub struct StoreWatchlistCoins<DA, CM> {
+    pub data_access: DA,
+    pub coin_market: CM,
+}
+
+impl<DA, CM> Interactor<StoreWatchlistCoinsRequest, ()> for StoreWatchlistCoins<DA, CM>
+where
+    DA: StoreWatchlistCoinsDataAccess,
+    CM: CoinMarket,
+{
+    async fn perform(&mut self, request: StoreWatchlistCoinsRequest) -> Result<(), Error> {
+        /* TODO:
+            - Check for repeated coins
+        */
+        let mut watchlist = self.data_access.get_watchlist().await?;
+        let usd_pairs = self.coin_market.fetch_usd_pairs().await?;
+        for coin in &request.coins {
+            for usd_pair in &usd_pairs {
+                if usd_pair.comparison.eq(coin) {
+                    watchlist.pairs.push(Pair {
+                        id: Uuid::new_v4().to_string(),
+                        base: String::from("USD"),
+                        comparison: coin.clone(),
+                        value: usd_pair.value.clone(),
+                        created_at: Utc::now().to_rfc3339(),
+                        updated_at: Utc::now().to_rfc3339(),
+                    });
+                    break;
+                }
+            }
+        }
+        self.data_access.update_watchlist(&watchlist).await?;
+        return Ok(());
+    }
+}
+
+#[cfg(test)]
+mod test {
+    // TODO: write tests for this `impl``
+}
